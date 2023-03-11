3df9eb703e01b0b61a263526f1bc8cab06c7e9d1
Moises-llanos <Davidllanos095@hotmail.com>
2023-03-10 20:28:15 -0500

    new avances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { LottieModule } from 'ngx-lottie';
 import player from 'lottie-web';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
@@ -17,7 +18,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         AppRoutingModule, 
         HttpClientModule, 
         BrowserModule,
-
+        SharedModule
       ],
   providers: [],
   bootstrap: [AppComponent]
