7aad867a66726f218f477113b958f128b814e5f7
moises-llanos <Davidllanos095@hotmail.com>
2023-03-15 10:11:23 -0500

    new avances..

--- src/app/shared/components/title-won/title-won.component.ts
+++ src/app/shared/components/title-won/title-won.component.ts
@@ -1,13 +1,17 @@
-import { Component, Input } from "@angular/core";
+import { AfterViewChecked, Component, Input } from "@angular/core";
 @Component({
     selector: 'app-title-won',
     templateUrl: './title-won.component.html',
     styleUrls: ['./title-won.component.scss']
 })
 
-export class TitleWonComponent {
+export class TitleWonComponent implements AfterViewChecked {
     @Input() canWon: boolean = false;
     public options = {
         path: 'assets/lottie/loader.json'
+    }
+
+    ngAfterViewChecked(): void {
+        
     }
 }
