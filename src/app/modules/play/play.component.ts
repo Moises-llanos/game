8009310421ddea34d3ecb5c5524bfec9e23b1670
Moises-llanos <Davidllanos095@hotmail.com>
2023-03-20 16:41:04 -0500

    corrigiendo cards

--- src/app/modules/play/play.component.ts
+++ src/app/modules/play/play.component.ts
@@ -85,7 +85,7 @@ export class PlayComponent implements OnInit, OnDestroy {
 
     const dataCopy = [...JSON.parse(JSON.stringify(data))];
     this.characters = [...data, ...dataCopy].sort(() => Math.random() - 0.5);
-    setTimeout(() => (this.canLoad = true), 1300);
+    setTimeout(() => {this.canLoad = true}, 1300);
   }
 
   won() {
