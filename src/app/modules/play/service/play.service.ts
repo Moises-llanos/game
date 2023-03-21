e21162db6dacbcbf5cbf81968a170f114a4077bc
moises-llanos <Davidllanos095@hotmail.com>
2023-03-21 17:21:53 -0500

    perspective

--- src/app/modules/play/service/play.service.ts
+++ src/app/modules/play/service/play.service.ts
@@ -4,9 +4,6 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from '@angular/core';
 import { map, Observable } from "rxjs";
 import { delay } from "rxjs/operators";
-
-
-
 @Injectable({
     providedIn: 'root'
 })
