9f0f52366723582a252dbe791fd7b5d9e89f1a9b
moises-llanos <Davidllanos095@hotmail.com>
2023-03-22 17:10:07 -0500

    new avances

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -33,10 +33,9 @@ export class CardComponent implements OnDestroy {
 
   changeImg() {
     if(!this.playService.totalMovimientos) this.onInterval.emit();
-    if(!this.backCard) this.backCard = true;
     const { status, active } = this.dataSrc;
     if (!status && !active && this.totalCards < 2) {
-      this.playService.totalMovimientos += 1
+      this.playService.totalMovimientos++
       this.playService.listCardsRef.push(this.dataSrc);
       this.dataSrc.status = true;
       if (this.totalCards === 2) this.compareCards();
@@ -54,13 +53,13 @@ export class CardComponent implements OnDestroy {
     this.playService.pares++;
     this.playService.pares === 10 ? this.setSrcAudio('won.mp3') : this.setSrcAudio('click.mp3');
     this.playService.listCardsRef.forEach((e) => e.active = true);
-    setTimeout(()=> this.playService.hasPoints = false, 800);
+    setTimeout(()=> this.playService.hasPoints = false, 700);
     this.playService.listCardsRef = [];
   }
 
   removeClass() {
     this.setSrcAudio('failed.mp3');
-    setTimeout(this.clearCards.bind(this), 900);
+    setTimeout(this.clearCards.bind(this), 700);
   }
 
   clearCards(){
