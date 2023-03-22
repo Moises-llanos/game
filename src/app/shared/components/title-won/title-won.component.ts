1af42724e587033cda74bb263e4164ca1e4465c8
moises-llanos <Davidllanos095@hotmail.com>
2023-03-22 16:08:59 -0500

    new avances

--- src/app/shared/components/title-won/title-won.component.ts
+++ src/app/shared/components/title-won/title-won.component.ts
@@ -7,16 +7,11 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 export class TitleWonComponent  {
     @Output() onNewPlay: EventEmitter<void> = new EventEmitter()
+    public gameOver = { path: 'assets/lottie/game-over.json'};
+    public options = { path: 'assets/lottie/loader.json'};
     @Input() isActive: boolean = false;
     @Input() isWinner: boolean = true;
     @Input() intentos: number = 0;
     @Input() pares: number = 0;
     @Input() time: number = 0;
-
-    public options = {
-        path: 'assets/lottie/loader.json'
-    }
-    public gameOver = {
-        path: 'assets/lottie/game-over.json'
-    }
 }
